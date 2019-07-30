60a2322f0f2fa6850199df89a9272023859da2c5
laurentws <laurent@websuit.fr>
2019-07-30 12:28:22 +0200

    service

--- src/app/liste/liste.component.ts
+++ src/app/liste/liste.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-liste',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _http: HttpService) { }
 
   ngOnInit() {
+    this._http.maMethode();
   }
 
 }
