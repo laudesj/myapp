2d5a2c4ad56c8a3f0fc0d283d6b0de76ea560a21
laurentws <laurent@websuit.fr>
2019-07-30 10:28:18 +0200

    accueil & liste routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AccueilComponent } from './accueil/accueil.component';
+import { ListeComponent } from './liste/liste.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: AccueilComponent },
+  { path: 'liste', component: ListeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { ListeComponent } from './liste/liste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ListeComponent
   ],
   imports: [
     BrowserModule,
